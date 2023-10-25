464bafeafe032fb9e750f68ff323f2d687d816e8
Aline Ciane Gomes <106564944+alinecianegomes@users.noreply.github.com>
2023-10-25 10:36:25 -0300

    Update BinarySearchTree.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -3,9 +3,19 @@ package tree;
 public class BinarySearchTree<T extends Comparable<T>> {
 
 	private Node<T> root;
-root.insertNode(newNode);
+
+	public BinarySearchTree() {
+		root = null;
 	}
 
+	public void insert(T obj) {
+		Node<T> newNode = new Node<T>(obj);
+		if (root == null)
+			root = newNode;
+		else
+			root.insertNode(newNode);
+	}
+	
 	public void inOrder() throws EmptyTreeException {
 		if (root != null) {
 			root.printNodesInOrder();
@@ -21,7 +31,7 @@ root.insertNode(newNode);
 			throw new EmptyTreeException();
 		}
 	}
-	
+
 	public void postOrder() throws EmptyTreeException {
 		if (root != null) {
 			root.printNodesPostOrder();
@@ -29,25 +39,85 @@ root.insertNode(newNode);
 			throw new EmptyTreeException();
 		}
 	}
-	public BinarySearchTree() {
-		root = null;
+	
+	public void remove(T element) {
+		root = remove(root, element);
 	}
 
-	public void insert(T obj) {
-		Node<T> newNode = new Node<T>(obj);
-		if (root == null)
-			root = newNode;
-		else
-			
+	public Node<T> remove(Node<T> node, T element) {
+		if (node == null) {
+			return node;
+		}
 
-	public void max(){
+		int current = element.compareTo(node.value);
 
+		if (current < 0) {
+			node.left = remove(node.left, element);
+		} else if (current > 0) {
+			node.right = remove(node.right, element);
+		} else {
+
+			if (node.left == null) {
+				return node.right;
+			} else if (node.right == null) {
+				return node.left;
+			}
+
+			node.value = minValue(node.right);
+
+			node.right = remove(node.right, node.value);
+		}
+		return node;
 	}
-	public void min(){
+
+	public T minValue(Node<T> node) {
+		T minValue = node.value;
+		while (node.left != null) {
+			minValue = node.left.value;
+			node = node.left;
+		}
+		return minValue;
+	}
+	
+	public T min() throws EmptyTreeException {
+		if (root == null) {
+			throw new EmptyTreeException();
+		}
+		return min(root).value;
+	}
+
+	private Node<T> min(Node<T> node) {
+		while (node.left != null) {
+			node = node.left;
+		}
+		return node;
+	}
+
+	
+	public T max() throws EmptyTreeException {
+		if (root == null) {
+			throw new EmptyTreeException();
+		}
+		return max(root).value;
+	}
+
+	private Node<T> max(Node<T> node) {
+		while (node.right != null) {
+			node = node.right;
+		}
+		return node;
+	}
+	
+	public void convertToVector() throws EmptyTreeException {
+
+				
+		if (root != null) {
+			root.convert();
+		} else {
+			throw new EmptyTreeException();
+		}
 		
 	}
-	public void Remove(){
-		
-	}
+
 
 }
